eacca3a3399ce143a003555eca76a0e6970e06f0
nathan101117 <124667960+nathan101117@users.noreply.github.com>
2025-02-26 14:11:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ if (!userName) {
 
 // Welcome Message
 alert("Welcome to EatNow cafe, " + userName + "!");
+
+// Display Menu Items
+let menu = ["Grilled Chicken", "Pasta Alfredo", "Veggie Burger", "Caesar Salad", "Tomato Soup"];
+console.log("EatNow Cafe Menu:");
+menu.forEach((dish, index) => {
+    console.log((index + 1) + ". " + dish);
+});
