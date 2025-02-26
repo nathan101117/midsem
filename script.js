dd992a063eb39256fd44593055b307cd19c71bdf
nathan101117 <124667960+nathan101117@users.noreply.github.com>
2025-02-26 14:15:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ console.log("EatNow Cafe Menu:");
 menu.forEach((dish, index) => {
     console.log((index + 1) + ". " + dish);
 });
+
+// Adding a New Dish
+let newDish = prompt("Suggest a new dish:");
+if (newDish) {
+    menu.push(newDish);
+    console.log("Updated Menu:", menu);
+} else {
+    console.log("No dish added.");
+}
